bb847faf2117edbddd5b3b9c5688d3b2e173d549
henri <henrievert32@gmail.com>
2024-01-29 15:14:54 +0200

    d

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace mobilcalul;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace mobilcalul;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
